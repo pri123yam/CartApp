046d9628bef292be75109e4fe358be64cd0ea20f
Priyam Guha <priyamguha@gmail.com>
2019-03-13 19:12:55 +0530

    fourth

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
+import Buton from './Button';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+class App extends React.Component {
+  constructor(props)
+  {
+    super(props);
+    this.state= {
+      val:0
+    }
   }
+  increase=()=>
+  {
+    console.log("inc "+ this.state.val)
+    this.setState(prevState=>
+    {
+      return {val: prevState.val + 1};
+    }
+      );
+  }
+  decrease=()=>{
+    this.setState(prevState=>
+      {
+        if(prevState.val>0)
+        return {val: prevState.val-1};
+      });
+  }
+render()
+{
+  return(
+    <div className='App'>
+        {<ul>
+          <Buton disp={'-'} click={()=>this.decrease()}/>
+          <span className='bord'>{this.state.val}</span>
+          <Buton disp={'+'} click={()=>this.increase()}/>
+        </ul>}
+    </div>
+  );
+}
 }
 
 export default App;
